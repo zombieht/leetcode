2d9df5f75c4a494cf55da360aeac78e0ba88a946
zombieht <zombie19931113@gmail.com>
2017-07-28 15:35:03 +0800

    add two numbers

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import TwoSum.TwoSum;
+
+/**
+ * Created by zombie on 2017/7/24.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] b = TwoSum.twoSums(new int[]{2, 3, 4, 5}, 8);
+        System.out.println(b[0] + " " + b[1]);
+    }
+
+
+}
--- src/main/java/TwoSum/TwoSum.java
+++ src/main/java/TwoSum/TwoSum.java
@@ -1,15 +1,8 @@
-/**
- * Created by zombie on 2017/7/24.
- */
+package TwoSum;
+
 public class TwoSum {
 
-    public static void main(String[] args) {
-        TwoSum a = new TwoSum();
-        int[] b = a.twoSums(new int[]{2, 3, 4, 5}, 8);
-        System.out.println(b[0] + " " + b[1]);
-    }
-
-    public int[] twoSums(int[] nums, int target) {
+    public static int[] twoSums(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
@@ -20,6 +13,5 @@ public class TwoSum {
         }
         throw new IllegalArgumentException("No two sum solution");
     }
-
 
 }
